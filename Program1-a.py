d5f8e0d5e9281da1881fab846da3c9a77f5dd42a
Renjimabl <145977478+Renjimabl@users.noreply.github.com>
2023-10-10 15:20:13 +0530

    Update Program1-a.py

--- Program1-a.py
+++ Program1-a.py
@@ -7,35 +7,3 @@
 r = int(input("enter the radius:"))
 area = 3.14*r*r
 print("area of the circle=",area)
-
-
-# In[1]:
-
-
-a = int(input("enter 1st number:"))
-b = int(input("enter 2nd number:"))
-c = int(input("enter 3rd number:"))
-if(a>b and a>c):
-    print("a is greater")
-elif(b>c and b>a):
-    print("b is greater")
-else:
-    print("c is greater")
-    
-
-
-# In[7]:
-
-
-n = int(input("enter a number:"))
-nn = str(n+n)
-nnn = str(n+n+n)
-sum = n+nn+nn
-print ("sum is",sum)
-
-
-# In[ ]:
-
-
-
-
